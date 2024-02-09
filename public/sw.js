e4f24dc29093d31640270b450d311cc453e8d8e8
raul <2021171023@uteq.edu.mx>
2024-02-09 17:19:39 -0600

    contenido

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,5 @@
 self.addEventListener('fetch', event => {
-    const offilineResponse = new Response(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1>Mode offiline</h1>
-            <p>Check your internet connection</p>
-        </body>
-        </html>
-    `, {
-        headers: {
-            'Content-Type': 'text/html'
-        }
+    const offilineResponse = new Response('pages/offline.html', {
     });
     const resp = fetch(event.request)
     .catch(() => offilineResponse)
