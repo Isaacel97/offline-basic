29d69392914455efbb7dc1bef3668fd4981cb579
raul <2021171023@uteq.edu.mx>
2024-02-09 17:19:04 -0600

    cambios

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,17 @@
 self.addEventListener('fetch', event => {
     const offilineResponse = new Response(`
-        <h1>Connection lost</h1>
-        <p>Check your internet connection</p>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Mode offiline</h1>
+            <p>Check your internet connection</p>
+        </body>
+        </html>
     `, {
         headers: {
             'Content-Type': 'text/html'
